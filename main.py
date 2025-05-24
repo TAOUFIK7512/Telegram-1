005070f9cb2ca9bf34e9cd6d5eca5d7ea5381bc4
TAOUFIK7512 <taoufikaitlouhi08@gmail.com>
2025-05-24 20:24:43 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+main.py
+
+
+   ```python
+   import requests
+   import time
+
+   TOKEN = "7357253957:AAEOY-xE8mrptYYpEDyYYP2Eve6ykMovzvs"
+   CHAT_ID = "7357253957"
+   MESSAGE = "✅ البوت شغال بنجاح على Render!"
+
+   def send_message():
+       url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+       data = {"7357253957": CHAT_ID, "text": MESSAGE}
+       requests.post(url, data=data)
+
+   if _name_ == "_main_":
+       while True:
+           send_message()
+           time.sleep(3600)  # كل ساعة
+   ```
